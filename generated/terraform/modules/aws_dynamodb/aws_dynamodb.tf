afc700075fffb7e8a81d840afe1b51392320f6ca
niraj8 <8666468+niraj8@users.noreply.github.com>
2024-08-30 07:13:29 +0000

    [create-pull-request] automated change

---
+++ generated/terraform/modules/aws_dynamodb/aws_dynamodb.tf
@@ -0,0 +1,63 @@
+# Create DynamoDB table
+resource "aws_dynamodb_table" "this" {
+  name           = var.table_name
+  billing_mode   = var.billing_mode
+  read_capacity  = var.read_capacity
+  write_capacity = var.write_capacity
+  hash_key       = var.hash_key
+  range_key      = var.range_key
+
+  dynamic "attribute" {
+    for_each = var.attribute
+    content {
+      name = attribute.value.name
+      type = attribute.value.type
+    }
+  }
+
+  dynamic "global_secondary_index" {
+    for_each = var.global_secondary_indexes
+    content {
+      name            = global_secondary_index.value.name
+      hash_key        = global_secondary_index.value.hash_key
+      range_key       = global_secondary_index.value.range_key
+      projection_type = global_secondary_index.value.projection_type
+      read_capacity   = global_secondary_index.value.read_capacity
+      write_capacity  = global_secondary_index.value.write_capacity
+    }
+  }
+
+  dynamic "local_secondary_index" {
+    for_each = var.local_secondary_indexes
+    content {
+      name            = local_secondary_index.value.name
+      range_key       = local_secondary_index.value.range_key
+      projection_type = local_secondary_index.value.projection_type
+    }
+  }
+
+  point_in_time_recovery {
+    enabled = var.point_in_time_recovery_enabled
+  }
+
+  server_side_encryption {
+    enabled = var.server_side_encryption_enabled
+  }
+
+  stream_enabled   = var.stream_enabled
+  stream_view_type = var.stream_view_type
+
+  tags = var.tags
+
+  dynamic "ttl" {
+    for_each = var.ttl
+    content {
+      attribute_name = ttl.value.attribute_name
+      enabled        = ttl.value.enabled
+    }
+  }
+}
+
+output "arn" {
+  value = aws_dynamodb_table.this.arn
+}
--- generated/terraform/outputs.tf
+++ generated/terraform/outputs.tf
@@ -1,10 +1,4 @@
 
-output "aws_s3_random_cat_photos_app_assets_bucket_website_endpoint" {
-  value = module.appcd_30685260-bc27-51e5-8156-12692f6da98f.bucket_website_endpoint
-  sensitive = false
-}
-
-
 output "aws_s3_random_cat_photos_app_assets_bucket_name" {
   value = module.appcd_30685260-bc27-51e5-8156-12692f6da98f.bucket_name
   sensitive = false
@@ -19,6 +13,12 @@ output "aws_s3_random_cat_photos_app_assets_arn" {
 
 output "aws_s3_random_cat_photos_app_assets_kms_arn" {
   value = module.appcd_30685260-bc27-51e5-8156-12692f6da98f.kms_arn
+  sensitive = false
+}
+
+
+output "aws_s3_random_cat_photos_app_assets_bucket_website_endpoint" {
+  value = module.appcd_30685260-bc27-51e5-8156-12692f6da98f.bucket_website_endpoint
   sensitive = false
 }
 
@@ -43,6 +43,12 @@ output "aws_lambda_hello_kitty_deploy_with_stackgen_function_invoke_arn" {
 
 output "aws_lambda_hello_kitty_deploy_with_stackgen_function_function_arn" {
   value = module.appcd_4c3ece38-8d24-5f08-8b3c-9afd84f1e112.function_arn
+  sensitive = false
+}
+
+
+output "aws_dynamodb_sample_table_arn" {
+  value = module.appcd_63261996-6469-487d-9133-8a522b04ad87.arn
   sensitive = false
 }
 
